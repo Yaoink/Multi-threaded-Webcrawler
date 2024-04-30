c96cf460930fbe8b88df75abaaa55e71c2d79fbb
Stephanie Hayden <shayden8@student.gsu.edu>
2024-04-29 20:19:47 -0400

    working pause and resume functionality have been added

--- MTWebCrawler/demo/src/main/java/com/example/MTWebCrawler.java
+++ MTWebCrawler/demo/src/main/java/com/example/MTWebCrawler.java
@@ -1,5 +1,5 @@
 package com.example;
-
+//import java.util.Scanner;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,7 +11,7 @@ public class MTWebCrawler implements Runnable{
   private static final int MAX_DEPTH = 3;
   private int ID;
   private Thread thread;
-  private boolean paused;
+  protected static boolean paused=false;
   private String first_link;
   private ArrayList<String> visitedLinks = new ArrayList<String>();
 
@@ -23,43 +23,87 @@ public class MTWebCrawler implements Runnable{
     System.out.println("Web Crawler "+ ID + " created.");
     this.first_link = first_link;
     this.ID = ID;
-    this.paused=false; //Default value of paused is false
+    //this.paused=false; //Default value of paused is false
     thread = new Thread(this);
     thread.start();
-  }//MTWebCrawler 
+    System.out.println("This is the thread id " +thread.getId());
+  }
+  
+ 
+  //MTWebCrawler 
   //A set pause method to make the private variable paused, publicly accessible
-  public synchronized void setPause(boolean pause){
+  /** public synchronized void setPause(boolean pause){
     this.paused=pause;
     notifyAll(); //Notify potentially waiting threads
-  }
-  //The synchronized keyword ensures that all threads exhibit the same behavior.
-  protected synchronized void checkPaused(){
-    while(paused){
+    protected synchronized void checkPaused(){
+    while(paused=false){
       try{
-        wait(); //Busy wait until pause is set to false
+        Thread.sleep(5000); //Busy wait until pause is set to false
       }
       catch(InterruptedException e){
         Thread.currentThread().interrupt();
       }
     }
+    
   }
+  } */
+ 
+  //The synchronized keyword ensures that all threads exhibit the same behavior.
+  /* public void handleUserInput() {
+    Scanner pauseOrResume= new Scanner(System.in);
+    System.out.println("Press p to pause the webcrawler, press r to resume the webcrawler");
+    
+        char input=pauseOrResume.next().charAt(0);
+        while(input=='p'){
+          paused=true;
+        }
+        if(input=='r'){
+          paused=false;
+        }
+        pauseOrResume.close();
+    } */
+ 
+
+    
+
   // Runs the web crawler
   @Override
   public void run(){
     crawl(1, first_link);
   }//run
+  private static final Object lock = new Object();
+
+  private static void getPause() {
+    synchronized (lock) {
+        while (paused) {
+            try {
+                lock.wait();  // Wait until notified
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+public static void setPause(boolean b) {
+  synchronized (lock) {
+      paused=b;
+      if (!paused) {
+          lock.notifyAll();
+      }
+  }
+}
 
   /** Recursively crawls webpages
       @param the current depth of the crawler
       @param the current link
   */
   private void crawl(int level, String url){
+    getPause();
     if (level <= MAX_DEPTH){
       Document doc = request(url);
 
       if (doc != null){
         for (Element link : doc.select("a[href]")){
-          checkPaused();
           String next_link = link.absUrl("href");
           if (!next_link.startsWith("javascript:")) {  // Check if it's not a JavaScript link that leads to nothing
                     if (!visitedLinks.contains(next_link)) {
@@ -93,8 +137,11 @@ public class MTWebCrawler implements Runnable{
       return null;
     } 
     catch(IOException e){
+      System.out.println("The webcralwer of "+ ID + " encountered an error of some kind");
+      e.printStackTrace();
       return null;
     }
+
   }//request
 
   /** Gets the thread object
--- MTWebCrawler/demo/src/main/java/com/example/Main.java
+++ MTWebCrawler/demo/src/main/java/com/example/Main.java
@@ -6,12 +6,15 @@ public class Main {
   
   
   public static void main(String[] args) {
+    System.out.printf("Minimum priority of the thread is '%s' \n", Thread.MIN_PRIORITY);
+    System.out.printf("Maxmimum priority of the thread is '%s' \n", Thread.MAX_PRIORITY);
+    System.out.printf("Normal priority of the thread is '%s' \n", Thread.NORM_PRIORITY);
     ArrayList<MTWebCrawler> bots = new ArrayList<>();
+    
+    //asks user for input
     ArrayList<String> links=new ArrayList<>();
-    //String command;
     String link;
     Scanner scanner = new Scanner(System.in);
-    // Create bots
     System.out.println("How many links would you like to explore?");
     int i=scanner.nextInt();
     scanner.nextLine();
@@ -20,24 +23,21 @@ public class Main {
       link=scanner.nextLine();
       links.add(link);
     }
+    KeyEventDemo demo=new KeyEventDemo("demo");
+    Thread inputThread=new Thread(demo);
+    inputThread.start();
+
+    //creates MTWebCrawler object, each MTWebCralwer starting link has its own thread of execution
     for(int k=1; k<=i; k++){
       bots.add(new MTWebCrawler(links.get(k-1), k));
     }
     
     scanner.close();
-    Scanner pauseOrResume= new Scanner(System.in);
-    System.out.println("Press p to pause the webcrawler, press r to resume the webcrawler");
-    char input=pauseOrResume.next().charAt(0);
-    if(input=='p'){
-      bots.get(0).setPause(true);
-    }
-    if(input=='r'){
-      bots.get(0).setPause(false);
-    }
+
     if(bots.size()==i){
-    System.out.println("Web crawler is running. Type 'pause' to pause, 'resume' to resume, 'exit' to quit.");
+    System.out.println("Web crawler is running. Type 'p' to pause execution, 'r' to resume execution and control c to quit");
     
-    // Start bots
+    
     for(MTWebCrawler bot : bots){
       try{
         bot.getThread().join();
@@ -48,10 +48,16 @@ public class Main {
     }
 
   }
-  String[] phrases=new String[]{"Whew, executed with no exceptions", "TFW you absolutely slayed this OS project", "I found all the links!", "I challenge you to find more links than this at depth level "+i+ " of course (;)",
-"Why's it called a webcrawler when I found "+ links.size()+ "in like two seconds?"};
+
+  //all the MTWebCrawler threads have been closed at this point
+  String[] phrases=new String[]{"Whew, executed with no exceptions", "TFW you absolutely slayed this OS project", "I found all the links!", "I challenge you to find more links than this (;))",
+"Why's it called a webcrawler when I found "+ links.size()+ "in like two seconds? Should be called a web sprinter"};
   int index=(int)(Math.random()*phrases.length);
   System.out.println(phrases[index]);
-  pauseOrResume.close();
-  }
+
+  
 }
+}
+
+
+
